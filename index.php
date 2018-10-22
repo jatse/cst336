bd78b139ce591e44f8a81ade697175106bee320d
Jason Tse <jatse@csumb.edu>
2018-10-22 19:23:57 +0000

    main directory links added

--- index.php
+++ index.php
@@ -1,11 +1,115 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
+<html>
+    <head>
+        <style type="text/css">
+            body{
+                text-align: center;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+            table{
+                margin: auto;
+                border-collapse: collapse;
+            }
+            td{
+                width: 100px;
+                height: 30px;
+                text-align: center;
+                font-size: 10pt;
+                border: 1px solid #AAAAAA;
+            }
+        </style>
+        <title>Jason Tse's CSUMB 336 Directory</title>
+    </head>
+    
+    <body>
+        <?php
+            // A simple web site in Cloud9 that runs through Apache
+            // Press the 'Run' button on the top to start the web server,
+            // then click the URL that is emitted to the Output tab of the console
 
-echo "Jason Tse's CST 336 Home Page";
-
-?>
-</body>
+            echo "Jason Tse's CST 336 Directory";
+        ?>
+        <br>
+        <hr width="28%" />
+        <br>
+        <table>
+            <tr>
+                <th>
+                    Homeworks
+                </th>
+                <th>
+                    Labs
+                </th>
+                <th>
+                    Projects
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <a href="#">Homework 1</a>
+                </td>
+                <td>
+                    <a href="#">Lab 1</a>
+                </td>
+                <td>
+                    <a href="#">Final</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="#">Homework 2</a>
+                </td>
+                <td>
+                    <a href="#">Lab 2</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="#">Homework 3</a>
+                </td>
+                <td>
+                    <a href="#">Lab 3</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="#">Homework 4</a>
+                </td>
+                <td>
+                    <a href="#">Lab 4</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="#">Homework 5</a>
+                </td>
+                <td>
+                    <a href="#">Lab 5</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="#">Homework 6</a>
+                </td>
+                <td>
+                    <a href="#">Lab 6</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="#">Homework 7</a>
+                </td>
+                <td>
+                    <a href="#">Lab 7</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="#">Homework 8</a>
+                </td>
+                <td>
+                    <a href="#">Lab 8</a>
+                </td>
+            </tr>
+        </table>
+    </body>
 </html>
