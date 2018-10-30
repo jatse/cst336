f534de4d396f0bb0bba9ec8d0a86d0879343b9eb
Jason Tse <jatse@csumb.edu>
2018-10-30 21:29:36 +0000

    lab 3 added; no controls

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -91,7 +91,7 @@
             <input type="submit" value="Submit"/>
         </form>
         
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>        
     </body>
 </html>
