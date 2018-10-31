5b5f2f468de7b858b2670f8aa8406315760fc042
Jason Tse <jatse@csumb.edu>
2018-10-31 01:29:27 +0000

    lab 3 edit form

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -1,13 +1,19 @@
 <?php
     $backgroundImage = "img/sea.jpg";
     
-    if(isset($_GET['keyword'])){
-        //echo "You searched for: " . $_GET['keyword'];
+    if(!empty($_GET['category'])){
         include 'api/pixabayAPI.php';
-        $imageURLs = getImageURLs($_GET['keyword'], $_GET['layout']);
+        $keyword = $_GET['category'];
+        $imageURLs = getImageURLs($keyword, $_GET['layout']);
+        $backgroundImage = $imageURLs[array_rand($imageURLs)];
+    }else if(!empty($_GET['keyword'])){
+        include 'api/pixabayAPI.php';
+        $keyword = $_GET['keyword'];
+        $imageURLs = getImageURLs($keyword, $_GET['layout']);
         $backgroundImage = $imageURLs[array_rand($imageURLs)];
     }
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -24,6 +30,29 @@
     
     <body>
         <br>
+        <!--Controls-->
+        <form>
+            <input type="text" name="keyword" placeholder="Keyword" value="<?=$keyword?>"/>
+            <div id="layoutDiv">
+                <input type="radio" id="lhorizontal" name="layout" value="horizontal">
+                <label for="Horizontal"></label><label for="lhorizontal">Horizontal</label>
+                <input type="radio" id="lvertical" name="layout" value="vertical">
+                <label for="Vertical"></label><label for="lvertical">Vertical</label>
+            </div>
+
+            <select name="category">
+                <option value="">Select One</option>
+                <option value="ocean">Sea</option>
+                <option value="forest">Forest</option>
+                <option value="mountain">Mountain</option>
+                <option value="snow">Snow</option>
+                <option value="desert">Desert</option>
+            </select>
+            <input type="submit" class="btn btn-default btn-sm" value="Submit"/>
+        </form>
+        
+        <br>
+        <!--Carousel-->
         <?php
             if(!isset($imageURLs)){
                 echo "<h2>Type a keyword to display a slideshow <br /> with random images from Pixabay.com </h2>";
@@ -58,7 +87,6 @@
                 ?>
             </div>
             
-            
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
@@ -72,25 +100,6 @@
         <?php
             } //END ELSE
         ?>
-        
-        <form>
-            <input type="text" name="keyword" placeholder="Keyword" value="<?=$GET['keyword']?>"/>
-            <div id="layoutDiv">
-                <input type="radio" id="lhorizontal" name="layout" value="horizontal">
-                <label for="Horizontal"></label><label for="lhorizontal">Horizontal</label>
-                <input type="radio" id="lvertical" name="layout" value="vertical">
-                <label for="Vertical"></label><label for="lvertical">Vertical</label>
-            </div>
-            <select name="keyword">
-                <option value="">Select One</option>
-                <option value="ocean">Sea</option>
-                <option value="forest">Forest</option>
-                <option value="mountain">Mountain</option>
-                <option value="snow">Snow</option>
-                <option value="desert">Desert</option>
-            </select>
-            <input type="submit" value="Submit"/>
-        </form>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>        
